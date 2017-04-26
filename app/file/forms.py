7f4fd7a15e79c9aee9faad146beb3ebf2cf16e9e
qun.su <alinsususu@163.com>
2017-04-26 20:55:51 +0800

    下载中心修改前

--- app/file/forms.py
+++ app/file/forms.py
@@ -1,5 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField
+from wtforms import FileField,SubmitField
+from flask_wtf.file import FileRequired,FileAllowed
+from flask_uploads import UploadSet
 
+files=UploadSet('files')
 class FileForm(FlaskForm):
-    file=FileField("上传文件")
+    file=FileField(validators=[FileAllowed(files,'上传默认类型'),FileRequired('文件未上传')])
+    submit=SubmitField('上传')
--- app/file/views.py
+++ app/file/views.py
@@ -1,7 +1,17 @@
 from flask import render_template,url_for
 from app.file import file
 from .forms import FileForm
+from flask_uploads import UploadSet
+from flask_uploads import UploadSet
+
+
+files=UploadSet('files')
 @file.route('/file')
 def file():
     form =FileForm()
+    if form.validate_on_submit():
+        filename=files.save(form.file.data)
+        file_url=files.url(filename)
+    else:
+        file_url=None
     return render_template('/file/file.html',form=form)
